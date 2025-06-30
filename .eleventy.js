7230d6e9bd43cb268e169141b9ae076a48730ff2
Brian454-nexus <brian.terer@student.moringaschool.com>
2025-06-30 15:48:46 +0300

    refactor: Update passthrough copy syntax in Eleventy config

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,23 @@
 const { DateTime } = require("luxon");
 
 module.exports = function (eleventyConfig) {
+  // Pass through static assets
+  eleventyConfig.addPassthroughCopy({
+    css: "css",
+    img: "img",
+    js: "js",
+    lib: "lib",
+    admin: "admin",
+  });
+
+  // Pre-process blog posts
+  eleventyConfig.addCollection("posts", function (collectionApi) {
+    return collectionApi.getFilteredByGlob("blog/*.md").sort(function (a, b) {
+      return b.date - a.date;
+    });
+  });
+
+  // Date formatting filter
   eleventyConfig.addFilter("postDate", (dateObj) => {
     return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED);
   });
@@ -11,5 +28,9 @@ module.exports = function (eleventyConfig) {
       includes: "_includes",
       output: "_site",
     },
+    passthroughFileCopy: true,
+    templateFormats: ["html", "njk", "md"],
+    htmlTemplateEngine: "njk",
+    markdownTemplateEngine: "njk",
   };
 };
