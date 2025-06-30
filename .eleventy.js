f24858e37a83ad675a65d7b6ff09f8010121b01e
Brian454-nexus <brian.terer@student.moringaschool.com>
2025-06-30 16:03:22 +0300

    docs: Update list formatting in Black Forest recipe

--- .eleventy.js
+++ .eleventy.js
@@ -2,13 +2,11 @@ const { DateTime } = require("luxon");
 
 module.exports = function (eleventyConfig) {
   // Pass through static assets
-  eleventyConfig.addPassthroughCopy({
-    "./css": "css",
-    "./img": "img",
-    "./js": "js",
-    "./lib": "lib",
-    "./admin": "admin",
-  });
+  eleventyConfig.addPassthroughCopy("css");
+  eleventyConfig.addPassthroughCopy("img");
+  eleventyConfig.addPassthroughCopy("js");
+  eleventyConfig.addPassthroughCopy("lib");
+  eleventyConfig.addPassthroughCopy("admin");
 
   // Pre-process blog posts
   eleventyConfig.addCollection("posts", function (collectionApi) {
@@ -22,20 +20,13 @@ module.exports = function (eleventyConfig) {
     return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED);
   });
 
-  // Custom filter for downcasing strings
-  eleventyConfig.addFilter("downcase", function (value) {
-    if (value && typeof value.toLowerCase === "function") {
-      return value.toLowerCase();
-    }
-    return value;
-  });
-
   return {
     dir: {
       input: ".",
       includes: "_includes",
       output: "_site",
     },
+    passthroughFileCopy: true,
     templateFormats: ["html", "njk", "md"],
     htmlTemplateEngine: "njk",
     markdownTemplateEngine: "njk",
