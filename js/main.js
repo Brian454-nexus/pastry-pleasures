e49a89329944802694ff00de071ef0ace98683b9
Brian454-nexus <brian.terer@student.moringaschool.com>
2025-07-04 01:59:48 +0300

    Added minor improv

--- js/main.js
+++ js/main.js
@@ -42,7 +42,7 @@
     items: 1,
     loop: true,
     autoplay: true,
-    autoplayTimeout: 4000,
+    autoplayTimeout: 6000,
     autoplayHoverPause: true,
     smartSpeed: 1000,
     animateOut: "fadeOut",
